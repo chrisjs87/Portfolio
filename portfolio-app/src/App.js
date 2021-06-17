24a8482e3a74b96dcc94e6ff3417303948c7f0f1
Christopher Scrantom <christopher.scrantom@gmail.com>
2021-06-17 18:18:57 -0500

    Small change

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p>This will be Chris Scrantom's Portfolio</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
